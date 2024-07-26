70afac5101aa0934317fad801eceded790eb7e91
Nicholas Honen <65134752+nickhonen@users.noreply.github.com>
2024-07-26 12:08:53 -0400

    setup app scaffolding and added dummy data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,25 @@
 import Link from "next/link";
 
+const mockUrls = [
+  "https://utfs.io/f/e1890d0e-aa07-4fed-a8b3-eff65cca462b-ft6l7l.png",
+  "https://utfs.io/f/0f66ec3b-f788-4931-ba0b-1bf09f30af86-qcuqdj.jpeg",
+];
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url,
+}));
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Hello test yo
+    <main>
+      <div className="flex flex-wrap gap-4">
+        {mockImages.map((image) => (
+          <div key={image.id} className="w-48">
+            <img src={image.url} alt="text"/>
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
