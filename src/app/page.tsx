52cc540f4cc415495bce3cec38a7849b494ca1d6
Nicholas Honen <65134752+nickhonen@users.noreply.github.com>
2024-08-12 12:43:13 -0400

    changed database to images, got rid of mock urls on homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,32 +3,16 @@ import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
-const mockUrls = [
-  "https://utfs.io/f/e1890d0e-aa07-4fed-a8b3-eff65cca462b-ft6l7l.png",
-  "https://utfs.io/f/0f66ec3b-f788-4931-ba0b-1bf09f30af86-qcuqdj.jpeg",
-];
-
-const mockImages = mockUrls.map((url, index) => ({
-  id: index + 1,
-  url,
-}));
-
 export default async function HomePage() {
-  const posts = await db.query.posts.findMany();
-
-  console.log(posts);
+  const images = await db.query.images.findMany();
 
   return (
     <main>
       <div className="flex flex-wrap gap-4">
-        {posts.map((post) => (
-          <div key={post.id} className="w-48">
-            {post.name}
-          </div>
-        ))}
-        {mockImages.map((image) => (
-          <div key={image.id} className="w-48">
-            <img src={image.url} alt="text"/>
+        {images.map((image, index) => (
+          <div key={index} className="flex w-48 flex-col gap-2">
+            <img src={image.url} alt="text" />
+            <div>{image.name}</div>
           </div>
         ))}
       </div>
--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -19,11 +19,12 @@ import {
  */
 export const createTable = pgTableCreator((name) => `val-lineup-dex_${name}`);
 
-export const posts = createTable(
-  "posts_table",
+export const images = createTable(
+  "images_table",
   {
     id: serial("id").primaryKey(),
-    name: varchar("name", { length: 256 }),
+    name: varchar("name", { length: 256 }).notNull(),
+    url: varchar("url", { length: 1024 }).notNull(),
     createdAt: timestamp("created_at", { withTimezone: true })
       .default(sql`CURRENT_TIMESTAMP`)
       .notNull(),
