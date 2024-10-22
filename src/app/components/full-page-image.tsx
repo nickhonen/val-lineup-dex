723d0010234a171b6d8f0b665dea51019146fa5f
Nicholas Honen <65134752+nickhonen@users.noreply.github.com>
2024-10-22 13:37:45 -0400

    routing/image page with parallel routes good enough

--- src/app/components/full-page-image.tsx
+++ src/app/components/full-page-image.tsx
@@ -1,15 +1,29 @@
 import { getImage } from "~/server/queries";
+import { auth } from "auth";
 
 export default async function FullPageImageView(props: { photoId: number }) {
+  const session = await auth();
   const image = await getImage(props.photoId);
+
   return (
-    <div className="flex h-full w-full min-w-0 bg-red-500">
+    <div className="flex h-full w-full min-w-0">
       <div className="flex-shrink">
-        <img src={image.url} className="w-96 object-contain" alt={image.name} />
+        <img
+          src={image.url}
+          className="flex-shrink object-contain"
+          alt={image.name}
+        />
       </div>
 
-      <div className="flex w-48 flex-shrink-0 flex-col">
-        <div className="text-xl font-bold">{image.name}</div>
+      <div className="border-1 flex w-48 flex-shrink-0 flex-col">
+        <div className="border-b p-2 text-center text-xl font-bold">
+          {image.name}
+        </div>
+
+        <div className="flex flex-col p-2">
+          <span>Uploaded by:</span>
+          <span>{session?.user.name}</span>
+        </div>
       </div>
     </div>
   );
