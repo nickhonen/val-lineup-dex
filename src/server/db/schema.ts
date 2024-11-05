19868433b24b9776125d219d060b9367b300b281
Nicholas Honen <65134752+nickhonen@users.noreply.github.com>
2024-11-04 19:51:48 -0500

    changed the db schema, havent pushed yet

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -4,11 +4,15 @@
 import { sql } from "drizzle-orm";
 import {
   index,
+  integer,
   pgTableCreator,
   serial,
   timestamp,
   varchar,
+  text,
 } from "drizzle-orm/pg-core";
+
+import type { InferSelectModel } from "drizzle-orm";
 
 export const createTable = pgTableCreator((name) => `val-lineup-dex_${name}`);
 
@@ -25,3 +29,26 @@ export const images = createTable(
   },
   (example) => [index("name_idx").on(example.name)],
 );
+
+// beginning of lucia auth
+
+export const users = createTable("users_table", {
+  id: serial("id").primaryKey(),
+  firstName: varchar("first_name", { length: 256 }).notNull(),
+  lastName: varchar("last_name", { length: 256 }).notNull(),
+  email: varchar("email", { length: 256 }).notNull(),
+});
+
+export const sessions = createTable("sessions_table", {
+  id: text("id").primaryKey(),
+  userId: integer("user_id")
+    .notNull()
+    .references(() => users.id),
+  expiresAt: timestamp("expires_at", {
+    withTimezone: true,
+    mode: "date",
+  }).notNull(),
+});
+
+export type User = InferSelectModel<typeof users>;
+export type Session = InferSelectModel<typeof sessions>;
