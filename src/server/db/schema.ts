d0489f56c4773e7284c5c735966fabb677925c70
Nicholas Honen <65134752+nickhonen@users.noreply.github.com>
2024-11-04 19:24:48 -0500

    changed schema to add RLS

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -14,12 +14,6 @@ import {
 } from "drizzle-orm/pg-core";
 import { anonRole, authenticatedRole } from "drizzle-orm/supabase";
 
-/**
- * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
- * database instance for multiple projects.
- *
- * @see https://orm.drizzle.team/docs/goodies#multi-project-schema
- */
 export const createTable = pgTableCreator((name) => `val-lineup-dex_${name}`);
 
 export const images = createTable(
