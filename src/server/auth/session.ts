9a1f8574c8dfa73ac2bab7c6c4d2ded91cb57ac3
Nicholas Honen <65134752+nickhonen@users.noreply.github.com>
2024-11-04 19:54:20 -0500

    started file for tomorrow, leaving off at create your api section

---
+++ src/server/auth/session.ts
@@ -0,0 +1,21 @@
+import type { User, Session } from "../db/schema";
+
+export function generateSessionToken(): string {
+	// TODO
+}
+
+export async function createSession(token: string, userId: number): Promise<Session> {
+	// TODO
+}
+
+export async function validateSessionToken(token: string): Promise<SessionValidationResult> {
+	// TODO
+}
+
+export async function invalidateSession(sessionId: string): Promise<void> {
+	// TODO
+}
+
+export type SessionValidationResult =
+	| { session: Session; user: User }
+	| { session: null; user: null };
